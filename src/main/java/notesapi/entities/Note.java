635e08a2e79b9418454453e69236adeee1630eda
Esperanza Macarena Avilés <esperanzamacarenaaviles@gmail.com>
2025-05-21 12:25:34 +0200

    feat: add Note entity

---
+++ src/main/java/notesapi/entities/Note.java
@@ -0,0 +1,42 @@
+package notesapi.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "notes")
+public class Note {
+
+    @Id
+    private String id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Note() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
